b5e34f3d537f40820aa55acb86306baf2875a8cf
Henry Vega <hvega@mandu.com>
2017-11-15 18:58:23 -0500

    Ajustes CSS

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,6 +1,7 @@
 <?php
 
 /* @var $this \yii\web\View */
+
 /* @var $content string */
 
 use yii\helpers\Html;
@@ -10,22 +11,22 @@ AppAsset::register($this);
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
-<html lang="<?= Yii::$app->language ?>">
+<html lang="<?php echo Yii::$app->language ?>">
 <head>
-    <meta charset="<?= Yii::$app->charset ?>">
+    <meta charset="<?php echo Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
-    <?= Html::cssFile(YII_DEBUG ? '@web/css/all.css' : '@web/css/all.min.css?v=' . filemtime(Yii::getAlias('@webroot/css/all.min.css'))) ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <?php echo Html::csrfMetaTags() ?>
+    <?php echo Html::cssFile(YII_DEBUG ? '@web/css/eqatu.min.css' : '@web/css/eqatu.min.css?v=' . filemtime(Yii::getAlias('@webroot/css/eqatu.min.css'))) ?>
+    <title><?php echo Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
 
-<?= $content ?>
+<?php echo $content ?>
 
-<?= Html::jsFile('@web/js/all.js?v=' . filemtime(Yii::getAlias('@webroot/js/all.js'))) ?>
+<?php echo Html::jsFile('@web/js/all.js?v=' . filemtime(Yii::getAlias('@webroot/js/all.js'))) ?>
 <?php $this->endBody() ?>
 </body>
 </html>
