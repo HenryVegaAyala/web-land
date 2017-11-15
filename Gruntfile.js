8db313eef2e47db094f6edaafdcdb524aa74a045
Henry Vega <hvega@mandu.com>
2017-11-15 18:48:44 -0500

    Ajustes CSS

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,28 @@
+/* eslint-disable quotes */
 module.exports = function (grunt) {
   grunt.initConfig({
+    concat_css: {
+      all: {
+        src: [
+          'web/lib/bootstrap/css/bootstrap.min.css',
+          'web/lib/font-awesome/css/font-awesome.min.css',
+          'web/lib/animate/animate.min.css',
+          'web/css/style.min.css'
+        ],
+        dest: "web/css/eqatu.css"
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'web/css/',
+          src: ['eqatu.css'],
+          dest: 'web/css/',
+          ext: '.min.css'
+        }]
+      }
+    },
     less: {
       dev: {
         options: {
@@ -30,17 +53,6 @@ module.exports = function (grunt) {
         }
       }
     },
-    cssmin: {
-      target: {
-        files: [{
-          expand: true,
-          cwd: 'web/css/',
-          src: ['style.css'],
-          dest: 'web/css/',
-          ext: '.min.css'
-        }]
-      }
-    },
     minified: {
       files: {
         src: ['web/js/all.js'],
@@ -49,6 +61,16 @@ module.exports = function (grunt) {
       options: {
         sourcemap: true,
         allinone: false
+      }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          cwd: 'web/img/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'web/img/'
+        }]
       }
     },
     watch: {
@@ -66,18 +88,8 @@ module.exports = function (grunt) {
           livereload: true
         }
       }
-    },
-    imagemin: {
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'web/img/',
-          src: ['**/*.{png,jpg,gif}'],
-          dest: 'web/img/'
-        }]
-      }
     }
-  })
+  }),
 
   grunt.loadNpmTasks('grunt-concat-sourcemap'),
   grunt.loadNpmTasks('grunt-contrib-watch'),
@@ -85,7 +97,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-minified'),
   grunt.loadNpmTasks('grunt-contrib-cssmin'),
   grunt.loadNpmTasks('grunt-contrib-imagemin'),
+  grunt.loadNpmTasks('grunt-concat-css'),
 
-  grunt.registerTask('build', ['less', 'concat_sourcemap', 'minified', 'cssmin', 'imagemin']),
+  grunt.registerTask('build', ['concat_css', 'cssmin', 'less', 'concat_sourcemap', 'minified', 'imagemin']),
   grunt.registerTask('default', ['watch'])
 }
